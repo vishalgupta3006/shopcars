2a9719719fe99b6e445fceff820fc84946040b26
Vishal Gupta <vishalgupta3006@gmail.com>
2021-09-26 17:39:03 +0530

    Crated reusable input fields

---
+++ src/components/reusable/SignupForm.js
@@ -0,0 +1,22 @@
+import Button from './Button';
+import InputField from './InputField';
+import './SignupForm.css';
+export default function SignupForm () {
+    const signupAction = (e) =>{
+        e.preventDefault();
+        console.log("From the sign up form ");
+    }
+    return (
+        <div className="SignupFormWrapper">
+            <form className="signupForm">
+                <InputField type="text" placeholder="Enter First Name" label = "First Name" id="signupFormFirstName"/>    
+                <InputField type="text" placeholder="Enter Last Name" label = "Last Name" id="signupFormLastName"/>
+                <InputField type="email" placeholder="Enter Email" label = "Email"  id="signupFormEmail"/>
+                <InputField type="tel" placeholder="Enter Mobile Number" label = "Mobile Number" id="signupFormPhone"/>
+                <InputField type="password" placeholder="Enter Password" label = "Password" id="signupFormPassword"/>
+                <InputField type="password" placeholder="Confirm Password" label = "Confirm Password" id="signupFormConfirmPassword"/>
+                <Button type="submit" label="Create Account" className="default" onclickhandler={signupAction}/>
+            </form>
+        </div>
+    );
+}
